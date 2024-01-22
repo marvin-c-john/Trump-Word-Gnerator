ac3c5aff95508315768048197975116503139302
yungeenMJ <127711513+yungeenMJ@users.noreply.github.com>
2024-01-22 18:36:58 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-const button = document.querySelector(".quote-button")
-const displayQuote = document.querySelector(".quote-container")
-const tweetButton = document.querySelector(".tweet-button")
+const button = document.querySelector(".quote-button");
+const displayQuote = document.querySelector(".quote-container");
+const tweetButton = document.querySelector(".tweet-button");
 
-
-button.addEventListener("click", getQuote)
-
-
-
-
+button.addEventListener("click", getQuote);
 
 function getQuote() {
-    fetch("https://api.whatdoestrumpthink.com/api/v1/quotes/random")
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data.message)
-            const quote = data.message
-            displayQuote.innerHTML = quote
-            tweetButton.setAttribute('href', `https://twitter.com/intent/tweet?text=${quote}`);
-        });
-
-
+  fetch("https://api.whatdoestrumpthink.com/api/v1/quotes/random")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.message);
+      const quote = data.message;
+      displayQuote.innerHTML = quote;
+      tweetButton.setAttribute(
+        "href",
+        `https://twitter.com/intent/tweet?text=${quote}`
+      );
+    });
 }
-
-
-
-
 
 //   const tweetInput = "https://twitter.com/intent/tweet?text="
 //   window.open(`${tweetInput}${quote}`);
